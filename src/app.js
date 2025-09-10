0b2af4c40e8a1dc126ef589a66f6761d77bec5b0
sirWerq <rifqx1904@gmail.com>
2025-09-10 15:00:36 +0700

    fix: update cors error

--- src/app.js
+++ src/app.js
@@ -10,8 +10,12 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
     cors({
-        origin: ["http://localhost:5173", "shortlink-amia.up.railway.app"],
+        origin: [
+            "http://localhost:5173",
+            "https://shortlink-amia.up.railway.app",
+        ],
         methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true,
     })
 );
 
